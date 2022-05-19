8da45867f9707475fa497e5c0a4b8b9236ced572
전승원 <jeonseungwon@jeonseung-wons-MacBook-Pro.local>
2022-05-19 21:33:50 +0900

    Update Question List Page and Analyze Page

--- client/src/components/QuestionItemList.js
+++ client/src/components/QuestionItemList.js
@@ -1,46 +1,40 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import QuestionItem from "./QuestionItem";
 
+const QuestionItemList = ({ title, questionList, setQuestionList }) => (
+  <div className="list">
+    {/* props로부터 title 값을 전달 받음 */}
+    <p className="list-title">{title}</p>
+    <ul className="list-ul">
+      {questionList && //questionlist가 있을 때만 출력
+        questionList.map((questionItem) => {
+          //삭제한 질문인 경우 출력하지 않음(deleted가 true)
+          if (questionItem.deleted) return null;
 
-const QuestionItemList = ({title,questionList,setQuestionList, checkedList}) =>(
-    <div className="list">
-        {/* {props로부터 title 값을 전달 받음} */}
-        <p className="list-title">{title}</p>    
-
-        <ul className="list-ul">
-            {questionList && //questionlist가 있을 때만 출력
-            questionList.map((questionItem) => {
-                //삭제한 질문인 경우 출력하지 않음(deleted가 true)
-                if(questionItem.deleted) return null;
-
-                //checkedList값에 따라 할일 목록 또는 완료한 목록을 출력
-                if(checkedList !== questionItem.checked) return null;
-
-                return(
-                    //map을 이용하여 QuesrionItem 출력
-                    <QuestionItem
-                    key={questionItem.id}
-                    questionItem={questionItem}
-                    questionList={questionList}
-                    setQuestionList={setQuestionList}
-                    />
-                );
-            })}
-        </ul>
-    </div>
+          return (
+            //map을 이용하여 QuestionItem 출력
+            <QuestionItem
+              key={questionItem.id} //빠르게 렌더링 하기 위해 key값 사용
+              questionItem={questionItem}
+              questionList={questionList}
+              setQuestionList={setQuestionList}
+            />
+          );
+        })}
+    </ul>
+  </div>
 );
 
 QuestionItemList.propTypes = {
-    title: PropTypes.string.isRequired,
-    questionList: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired,
-        })
-    ),
-    setQuestionList: PropTypes.func.isRequired,
-    checkedList: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+  questionList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ),
+  setQuestionList: PropTypes.func.isRequired,
 };
 
-export default QuestionItemList
+export default QuestionItemList;
--- client/src/pages/Question.js
+++ client/src/pages/Question.js
@@ -1,30 +1,66 @@
 import React, { useState } from "react";
 import InputBox from "../components/InputBox";
 import QuestionItemList from "../components/QuestionItemList";
-//import { Link } from "react-router-dom";
 import "../styles/Question.css";
+import { FcDatabase, FcList,FcGraduationCap, FcVoicePresentation, FcOrganization, FcIdea} from "react-icons/fc";
 
 const Question = () => {
-    const [questionList, setQuestionList] = useState([]);
+  const [questionList, setQuestionList] = useState([]); //useState 훅을 사용하여 item을 담을 리스트와 setter함수 생성
 
-    return (
-        <div className="Question"  style={{ backgroundImage: "url(./background-img.jpg)"}}>
-            <div className="question__container">
-                {/* 아이템을 추가할 수 있는 inputbox */}
-                <h2 class="QuestionHeader">연습할 면접 질문을 관리하세요</h2>
-                <InputBox questionList={questionList} setQuestionList={setQuestionList}/>
-
-                {/* 면접질문 Item 리스트 */}
-                <QuestionItemList 
-                    title={"면접 질문 리스트"}
-                    questionList={questionList}
-                    setQuestionList={setQuestionList}
-                    checkedList={false} //체크되지 않은 할 일 목록
-                />
-            </div>
+  return (
+    <div
+      className="Question"
+      style={{ backgroundImage: "url(./background-img.jpg)" }}
+    >
+      <div className="question__container">
+        {/* 아이템을 추가할 수 있는 inputbox */}
+        <h2 class="QuestionHeader">연습할 면접 질문을 관리하세요</h2>
+        <InputBox
+          questionList={questionList}
+          setQuestionList={setQuestionList}
+        />
+        <div class="category_contatiner">
+          <p class="cateheader">면접 유형 카테고리</p>
+          <div class="catediv">
+          {/* <button
+              type="button"
+              className="item-edit-btn"
+              onClick={onClickEditBtn}
+            >
+              <FcSettings size="20" />
+            </button> */}
+          <button class="cate" id="cate1" >
+            <FcDatabase size="25" /><br></br>
+            전체
+          </button>
+          <button class="cate" id="cate2">
+            <FcGraduationCap size="25" /><br></br>
+            역량
+          </button>
+          <button class="cate" id="cate3">
+            <FcVoicePresentation size="25" /><br></br>
+            지원동기
+          </button>
+          <button class="cate" id="cate4">
+            <FcOrganization size="25" /><br></br>
+            직무
+          </button>
+          <button class="cate" id="cate5">
+            <FcIdea size="25" /><br></br>
+            기초 인성
+          </button>
+          </div>
         </div>
-    );
-};
 
+        {/* 면접질문 Item 리스트 */}
+        <QuestionItemList
+          title={"면접 질문 리스트"}
+          questionList={questionList}
+          setQuestionList={setQuestionList}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Question;
