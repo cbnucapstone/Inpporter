d5097a63516a4e9c9a74284fcdd76d0a107cfc83
baeleekang <50096266+baeleekang@users.noreply.github.com>
2022-08-29 21:06:40 +0900

    이보현/ result 페이지 디자인

--- client/src/pages/Result.js
+++ client/src/pages/Result.js
@@ -23,7 +23,7 @@ const options = {
   transitionDuration: 1000
 };
 
-const size = [500, 300]; // 크기 조정
+//const size = [500, 300]; // 크기 조정
 
 function Result(){
 const location = useLocation();
@@ -32,7 +32,56 @@ words = location.state.word;
 
     return(
         <div className="Result"  style={{ backgroundImage: "url(./background-img.jpg)"}}>  
-        <ReactWordcloud size={size} options={options} words={words} />
+            <div id="title"><h2> 면접 분석 결과 </h2></div>
+            <div class="info">
+                <div id="area">
+                    <div class="entity"> <div class="type">이름</div> <div class="content">구구즈</div> </div>
+                    <div class="entity"> <div class="type">검사 일시</div> <div class="content">2022년 05월 24일</div>  </div>
+                    <div class="entity"> <div class="type">분야</div> <div class="content">역량</div>  </div>
+                </div>
+            </div>
+            <div class="resultArea">
+                <div class="btns">
+                    <button class="btn">질문 1</button>
+                </div>
+                <div class="btns">
+                    <button class="btn">질문 2</button>
+                </div>
+                <div id="question">
+                    질문 1
+                    <br />
+                    <li id="quesList">
+                        <ul id="quesContent"> 질문입니다. </ul>
+                    </li>
+                </div>
+                <div class="hr"></div>
+                <div id="vision">
+                    <div class="subtitle"><h3> Vision Analysis </h3></div>
+                    <div id="interviewVideo"></div>
+                    <div id="emotionGraph"></div>
+                </div>
+                <div class="hr"></div>
+                <div id="voice">
+                    <div class="subtitle"><h3> 음성 분석 </h3></div>
+                    <div id="one"></div>
+                    <div id="two"></div>
+                </div>
+                <div class="hr"></div>
+                <div id="word">
+                    <div class="subtitle"><h3> Word Frequency </h3></div>
+                    <div id="explain">구구즈 님의 면접 시 단어 사용 빈도 결과입니다.</div>
+                    <div id="three"><ReactWordcloud options={options} words={words} />
+                    </div>
+                    <div id="four"></div>
+                </div>
+                <div class="hr"></div>
+                <div id="pitch">
+                    <div class="subtitle"><h3> Voice Pitch </h3></div>
+                    <div id="five"></div>
+                    <div id="six"></div>
+                </div>
+            </div>
+
         </div>
     );
 }
