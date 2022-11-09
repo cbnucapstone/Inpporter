8abd733d26fa1daa8be5f4d42850f2da48f0dc14
“s2eung1” <jijking1209@naver.com>
2022-11-09 09:21:21 +0900

    npm build test

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -19,7 +19,7 @@ function Home() {
       <div className="home">
         <div className="leftSide">
           <h1>
-            최고의 인터뷰를 위한<br></br>서포터, 인포터
+            최고의 asdfasdfasdfasdfaf 위한<br></br>서포터, 인포터
           </h1>
           <p>AI 분석을 통한 면접 연습으로 꿈의 회사에 한걸음 더 다가가세요!</p>
           {login ? (
